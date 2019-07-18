0241d7a89feb333de92c6a962f67745d239ef05f
snehasis007 <snehasis007@gmail.com>
2019-07-18 19:59:44 +0530

    update

--- src/main/java/com/cts/taskmanager/Application.java
+++ src/main/java/com/cts/taskmanager/Application.java
@@ -5,19 +5,25 @@ import java.util.Arrays;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class Application implements CommandLineRunner{
+public class Application extends SpringBootServletInitializer{
+//	public static void main(String[] args) {
+//        SpringApplication.run(Application.class, args);
+//    }
 	public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
 	
-	@Override
-	public void run(String... args) throws Exception {
-		
-	}
 //	@Bean
 //    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
 //        return args -> {
